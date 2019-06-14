cdf47732071de7063bbe327236a80dc4ee28f08c
jUnion44 <jUnion444@gmail.com>
2019-06-14 17:44:36 -0400

    3

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TextPage(models.Model):
+    title = models.CharField(max_length=200)
+    shortTitle = models.CharField(max_length=200)
+    content = models.TextField()
+    def __str__(self):
+        return self.shortTitle
