c8b1d188ee59875890ff8ae47d29a2dbfba805ee
Patrick Gough <jUnion444@gmail.com>
2019-06-15 21:29:08 -0400

    file upload system

---
+++ core/forms.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import *
+# Create your views here.
+from django import forms
+
+def valFile(val):
+    pass
+
+
--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import *
+from .forms import *
 # Create your views here.
 def index(request):
     #Render links IN ORDER!
     return render(request,"core/home.html",{"objects":TextPage.objects.order_by('numId')})
+
+def processFile(request):
+    file = request.FILES["file"]
+    File(file=file).save()
+    return HttpResponseRedirect(reverse("core:index"))
