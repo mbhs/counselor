da3351461ddd85438de2c9c8a1ab6c22ed2dff94
Patrick Gough <patrick@ptgough.com>
2019-12-02 19:19:14 +0000

    Events page displays events in a one week frame

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import *
 from .forms import *
+import datetime
 # Create your views here.
 def index(request):
     #Render links IN ORDER!
@@ -25,4 +26,13 @@ def contentPage(request,st):
     return render(request,"core/content.html",{"objects":TextPage.objects.order_by('numId'),"text":get_object_or_404(TextPage,shortTitle=st )})
 
 def listEvents(request):
-    return render(request,"core/calendar.html",{"objects":TextPage.objects.order_by('numId'),"events":Event.objects.order_by("date")})
+    today = datetime.date.today()
+    d = today
+    while d.weekday() != 1:
+        d = d - datetime.timedelta(days=1)
+
+    e = Event.objects.filter(date=datetime.date.today())
+    f = []
+    for count in range(7):
+        f = f + list(Event.objects.filter(date=(d+datetime.timedelta(days=count))))
+    return render(request,"core/calendar.html",{"objects":TextPage.objects.order_by('numId'),"events":f})
